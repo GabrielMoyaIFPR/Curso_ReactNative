f8e5414ce41fcda5163e79b172c0acb34cc3595d
Gabriel Moya <gabrielmoya201415@gmail.com>
2022-04-05 18:16:23 -0300

    Trabalhando com comunicação Direta e Indireta

--- AwesomeProject/src/App.js
+++ AwesomeProject/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import Contador from './components/Contador';
+import ContadorV2 from './components/contador/ContadorV2';
+//import Pai from './components/indireta/Pai';
+//import Pai from './components/direta/Pai';
+//import Contador from './components/Contador';
 //import Botao from './components/Botao';
 //import Titulo from './components/Titulo';
 //import Aleatorio from './components/Aleatorio';
@@ -11,9 +14,11 @@ import Contador from './components/Contador';
 
 export default () => (
     <View style={style.App}>
+        <ContadorV2 />
+        {/* 
+        <Pai/>
         <Contador inicial={100} passo={13} />
         <Contador/>
-        {/* 
         <Botao/>
         <Titulo principal="Cadastro"
             secundario="Tela de Cadastro" />
