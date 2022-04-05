66fb23cb2755d37398161b38bb984ed30e0d289e
Gabriel Moya <gabrielmoya201415@gmail.com>
2022-04-05 14:54:14 -0300

    Finalizando primeiros desafios

--- AwesomeProject/src/App.js
+++ AwesomeProject/src/App.js
@@ -1,6 +1,33 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import Aleatorio from './components/Aleatorio';
+// import Primeiro from './components/Primeiro';
+// import CompPadrao, {Comp1, Comp2} from './components/Multi';
+// import MinMax from './components/MinMax';
 
-import Primeiro from './components/Primeiro';
 
-export default () => <View><Primeiro/></View>
+export default () => (
+    <View style={style.App}>
+        <Aleatorio min={1} max={60}/>
+        <Aleatorio min={1} max={60}/>
+        <Aleatorio min={1} max={60}/>
+        <Aleatorio min={1} max={60}/>
+        <Aleatorio min={1} max={60}/>
+        <Aleatorio min={1} max={60}/>
+        {/* <MinMax min={3} max={20}/>
+        <MinMax min={20} max={75}/> */}
+        {/* <CompPadrao />
+        <Comp1 />
+        <Comp2 />
+        <Primeiro/> */}
+    </View>
+);
+
+const style = StyleSheet.create({
+    App: {
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    }
+});
