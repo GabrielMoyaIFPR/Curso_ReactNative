86e433006fea96878ad45fea16e7a963057b2695
Gabriel Moya <gabrielmoya201415@gmail.com>
2022-04-22 10:33:54 -0300

    Finalizando Navigation com Tabs

--- navegacao/src/views/TelaC.js
+++ navegacao/src/views/TelaC.js
@@ -3,10 +3,14 @@ import TextoCentral from "../components/TextoCentral";
 
 
 export default props => {
-    const route = props.route || { params: {numero: 0}}
+    const numero = props.route 
+            && props.route.params 
+            && props.route.params.numero 
+            ? props.route.params.numero : 0;
+
     return (
     <TextoCentral corFundo='#9932cd'>
-        Tela C - {route.params.numero}
+        Tela C - {numero}
     </TextoCentral>
     );
 };
